b71124d530549bb8223c31f706e4f12e03f466ff
Jc Tarot <jctarot.gep@gmail.com>
2020-04-16 15:14:28 +0200

    page folder for home.twig & annotations

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,13 +2,26 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/",name="home_")
+ */
 
 class HomeController extends AbstractController
 {
+    /**
+     * @Route("/home",name="away")
+     * (routes.yaml avec /)
+     */
     public function index(){
-        return $this->render('home.html.twig');
+        return $this->render('pages/home.html.twig');
     }
-    // public function base(){
-    //     return $this->render('base.html.twig');
-    // }
+    /**
+     * @Route("/myagency",name="myagency")
+     * (routes.yaml avec /base) 
+     */
+    public function base(){
+        return $this->render('base.html.twig');
+    }
 }
